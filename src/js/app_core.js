612c6116c1d9552e2b874a1c67f4b56ce5dd9470
Smurdy <blaisesmurda@gmail.com>
2026-08-22 15:58:37 -0700

    1.6.3: Update version number

--- src/js/app_core.js
+++ src/js/app_core.js
@@ -2068,7 +2068,7 @@ if (!hasInitialQuiz) {
 // - small bugfix: increment third digit (1.0.1)
 // - add/remove feature: increment second digit (1.1.0)
 // - breaking change: increment first digit (2.0.0)
-const APP_VERSION = "1.6.2";
+const APP_VERSION = "1.6.3";
 
 function injectVersionBadge() {
     try {
